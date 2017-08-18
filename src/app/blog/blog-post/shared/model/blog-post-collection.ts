260422ab35f0d733f6cb2fce166849f10b4a32a4
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2017-08-18 12:41:20 +0200

    Simplified collection name

--- src/app/blog/blog-post/shared/model/blog-post-collection.ts
+++ src/app/blog/blog-post/shared/model/blog-post-collection.ts
@@ -2,5 +2,5 @@ import { HalCollection } from '../../../../core/shared/model/hal-collection';
 import { BlogPost } from './blog-post';
 
 export class BlogPostCollection extends HalCollection {
-  _embedded: { 'blogPosts': BlogPost[] };
+  _embedded: { 'posts': BlogPost[] };
 }
