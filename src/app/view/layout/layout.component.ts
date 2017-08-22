2cb5650d73fb0b4989b63badb0cf0db4b522649a
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2017-08-22 23:12:26 +0200

    Elevate the toolbar while scrolled

--- src/app/view/layout/layout.component.ts
+++ src/app/view/layout/layout.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'gb-layout',
   templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent { }
+export class LayoutComponent {
+  elevated$ = Observable.fromEvent(window, 'scroll')
+    .map((test) => window.scrollY > 0);
+}
